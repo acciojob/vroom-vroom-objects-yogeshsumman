900517e05f189f45b1a6d2c1ecd1f4e7646a2441
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-04-13 16:55:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add getMakeModel method to Car prototype
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+    // Call the parent constructor with the current instance
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
+}
+
+// Set up inheritance - SportsCar inherits from Car
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
